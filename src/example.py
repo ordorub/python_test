923d1e8f3c857fa811571d4c1afb6fa9691d82ea
Ivan <ordorub@ya.ru>
2024-10-30 14:52:36 +0700

    Update example.py

--- src/example.py
+++ src/example.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/getDate')
 def getDate():
-    return str(datetime.today())
+    return '"' + str(datetime.today()) + '"'
